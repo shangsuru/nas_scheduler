2d459b9d532bd96449f81be2c225e1bc6295d7c6
Lukas Schneider <lu.schneider@protonmail.com>
2020-12-27 16:20:01 -0500

    implemented the heartbeat method

--- heartbeat.py
+++ heartbeat.py
@@ -41,6 +41,39 @@ class Heartbeat(Handler):
     def heartbeat(self):
         """Performs a single heartbeat and updates the cluster and scheduler accordingly.
 
-        TODO: implement cluster heartbeat.
+        We detect dead nodes by checking whether the phase of all pods on the node is set to "Failed".
+        (See: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase)
         """
-        logger.debug(f"Heartbeat")
+        nodes = k8s_api.get_nodes()
+        erroneous_nodes = []
+
+        # Collect nodes that may have experienced an error
+        for node in nodes:
+            pods = k8s_api.get_pods(field_selector={"spec.nodeName": node.metadata.name})
+
+            if any(p.status.phase != "Failed" for p in pods):
+                continue
+
+            erroneous_nodes.append(node)
+
+        # Handle erroneous nodes
+        for node in erroneous_nodes:
+            logger.warn(f"Node {node.metadata.name} has failed.")
+
+            # Retrieve node address
+            try:
+                address = next(address for address in node.status.addresses if address.type == "InternalIP").address
+            except KeyError:
+                logger.error(f"Failed to get internal IP address of node {node.metadata.name}!")
+                continue
+
+            if address not in self.cluster.nodes:
+                logger.warn(f"Node {node.metadata.name} ({address}) has failed but is not in the cluster nodes list.")
+                continue
+
+            # Remove node from cluster
+            self.cluster.remove_node(address)
+
+            # Remove jobs running on node from running jobs in scheduler
+            running_jobs = self.scheduler.running_jobs
+            self.scheduler.running_jobs = [job for job in running_jobs if address not in job.worker_placement]
