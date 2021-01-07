523b4f45bd1e50595bbe3bf13a1bbe3b9c6e3f89
e3ntity <lu.schneider@protonmail.com>
2021-01-07 16:38:35 +0100

    adapted heartbeat to use asyncio and added the heartbeat to the daemon

--- heartbeat.py
+++ heartbeat.py
@@ -1,13 +1,13 @@
+import asyncio
 import time
 
-from communication import Handler, hub
 from k8s.api import KubeAPI
 from log import logger
 
 k8s_api = KubeAPI()
 
 
-class Heartbeat(Handler):
+class Heartbeat:
     """A heartbeat handler that regularly checks the availability of k8s as well as the pods.
 
     Attributes:
@@ -20,30 +20,25 @@ class Heartbeat(Handler):
     heartbeat_interval = 10
 
     def __init__(self, scheduler, cluster, daemon=True):
-        super().__init__(connection=hub.connection, entity="heartbeat")
-
         self.cluster = cluster
         self.scheduler = scheduler
         self.last_beat = 0
 
-    def on_iteration(self):
+    async def on_iteration(self):
         """Emits a heartbeat if the last heartbeat was at least heartbeat_interval seconds ago."""
         if time.perf_counter() - self.last_beat < self.heartbeat_interval:
             return
 
-        self.heartbeat()
+        await self.heartbeat()
         self.last_beat = time.perf_counter()
 
-    def process(self, message):
-        """Apart from the stop message (processed by handler), the Heartbeat class does not process any messages."""
-        pass
-
-    def heartbeat(self):
+    async def heartbeat(self):
         """Performs a single heartbeat and updates the cluster and scheduler accordingly.
 
         We detect dead nodes by checking whether the phase of all pods on the node is set to "Failed".
         (See: https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#pod-phase)
         """
+
         nodes = k8s_api.get_nodes()
         erroneous_pods = []     # List of pod names (pod.metadata.name) for pods that have failed.
         erroneous_nodes = []    # List of kubernetse node objects for nodes that have failed.
